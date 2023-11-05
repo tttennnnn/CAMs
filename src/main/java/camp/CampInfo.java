ef35c698605a34b1bdb1537f8710157f0255318c
tttennnnn <ttenn.sapol@gmail.com>
2023-11-06 02:45:54 +0700

    add new jar

---
+++ src/main/java/camp/CampInfo.java
@@ -0,0 +1,10 @@
+package camp;
+
+import java.util.Date;
+
+public class CampInfo {
+    private String name, location, description, staffID;
+    private Date startDate, endDate, registrationClosingDate;
+    private int totalSlots, committeeSlots;
+    private String[] students, committees;
+}
